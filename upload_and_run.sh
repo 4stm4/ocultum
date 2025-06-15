f550828eebc01a1bce2d5d623dfee5339cd17b08
Aleksei <4stm4.74rus@gmail.com>
2025-06-15 16:44:45 +0500

    changed upload_and_run.sh" "

--- upload_and_run.sh
+++ upload_and_run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -e
 
-# Обновление репозитория
 echo "=== git pull ==="
 git pull
 
-# Сборка проекта
 echo "=== build ==="
-cargo build --release --target=aarch64-unknown-linux-gnu
+cargo build --release
 
-# Загрузка и запуск на Raspberry Pi Zero 2W
-echo "=== upload and run ==="
-scp target/aarch64-unknown-linux-gnu/release/ocultum pi@raspberrypi:/home/pi/ocultum
-ssh pi@raspberrypi "chmod +x /home/pi/ocultum && /home/pi/ocultum"
+echo "=== run locally ==="
+./target/release/ocultum
